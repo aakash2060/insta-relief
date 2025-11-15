e4567125a6c6902f9cb9271ad66c5cef9215b468
subin <bistasubeen715@gmail.com>
2025-11-15 12:59:07 -0600

    Firebase Cloud Functions Added

--- insta-relief/functions/index.js
+++ insta-relief/functions/index.js
@@ -5,12 +5,14 @@ const logger = require("firebase-functions/logger");
 // Optional: controls cost
 setGlobalOptions({ maxInstances: 5 });
 
+// ---- SIMPLE TEST FUNCTION ----
 exports.helloWorld = onRequest((req, res) => {
   logger.info("HelloWorld function was called!");
 
   res.status(200).send("Hello from Firebase Functions!");
 });
 
+// ---- Another simple JSON test ----
 exports.testJson = onRequest((req, res) => {
   res.json({
     message: "Firebase functions working!",
